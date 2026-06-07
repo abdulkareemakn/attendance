bf8f0b3a81555cc7cf12f3d615cfab9c6abffe05
Abdul Kareem <abdulkareemakn@proton.me>
2026-06-07 21:34:41 +0500

    chore: added settings and env variable config

---
+++ app/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    session_secret: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int = 30
+    database_url: str
+    google_client_id: str
+    google_client_secret: str
+    resend_api_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()  # ty:ignore[missing-argument]
